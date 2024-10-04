236f8c51f5152190a3bd5a9fb0924fb4a0d8fb92
Kushwaha-aman024 <kushwahaaman024@gmail.com>
2024-10-04 16:44:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AddProduct from "./components/AddProduct"; // Adjust the path based on where AddProduct is located
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gradient-to-r from-blue-50 to-indigo-100 flex items-center justify-center">
+    <div className="w-full max-w-4xl mx-auto p-8 bg-white shadow-lg rounded-lg">
+      <h1 className="text-4xl font-bold mb-8 text-center text-indigo-700 tracking-wide">
+         
+        </h1>
+        {/* AddProduct Component */}
+        <AddProduct />
+      </div>
     </div>
   );
 }
